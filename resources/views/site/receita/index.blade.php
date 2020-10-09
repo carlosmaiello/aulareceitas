d5b5acaee40e0e2e3b20575c0043cd382071e754
Carlos Maiello <carlosmaiello@gmail.com>
2020-10-09 18:40:27 -0300

    Blade

--- resources/views/site/receita/index.blade.php
+++ resources/views/site/receita/index.blade.php
@@ -1,13 +1,15 @@
 <html>
+
 <body>
     <h1>{{ $titulo }}</h1>
 
     @foreach ($receitas as $receita)
         <h3>{{ $receita->titulo }}</h3>
         <h5>Ingredientes</h5>
-        <p>{{ $receita->ingredientes }}</p>    
+        <p>{{ $receita->ingredientes }}</p>
         <h5>Modo de Preparo</h5>
         <p>{{ $receita->modo_preparo }}</p>
     @endforeach
 </body>
+
 </html>
